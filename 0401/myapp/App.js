3193264d883ed9268c32dc818aef1ffb098e6c38
eaezzangE <guqwhd0703@gmail.com>
2026-04-01 11:13:43 +0900

    Add files via upload

---
+++ 0401/myapp/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyComponent from './MyComponent';
+function App() {
+  return (
+    <div>
+      <MyComponent name="HTML" />  
+      <MyComponent name="Javascript" />  
+      <MyComponent name="React" />
+    </div>
+  );
+}
+export default App;
